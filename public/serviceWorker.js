dae99e388cce5d094f58acb2801806c7e79c366d
Michael Ashfield <michaelashfield@Michaels-MacBook-Air.local>
2020-06-27 20:49:24 +0100

    Repaired service worker caching

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,7 @@
 var cacheName = "v1:static";
 
 //function register() {
-console.log("Registering service worker");
+console.log("Registering service worker v0.1");
 // during the install phase you usually want to cache static assets
 self.addEventListener("install", function(e) {
 	// once the SW is installed, go ahead and fetch the resources to make this work offline
@@ -10,9 +10,17 @@ self.addEventListener("install", function(e) {
 			caches.open(cacheName).then(function(cache) {
 				return cache
 					.addAll([
-						"/",
                         "/build/bundle.css",
-                        "/build/bundle.js",
+						"/build/bundle.js",
+						"/icons/android-chrome-192x192.png",
+						"/icons/android-chrome-256x256.png",
+						"/icons/apple-touch-icon.png",
+						"/icons/browserconfig.xml",
+						"/icons/favicon-16x16.png",
+						"/icons/favicon-32x32.png",
+						"/icons/favicon.ico",
+						"/icons/mstile-150x150.png",
+						"/icons/safari-pinned-tab.svg",
                         "/vendor/jquery/jquery.min.js",
                         "/vendor/bootstrap/css/bootstrap.min.css",
                         "/vendor/bootstrap/js/bootstrap.min.js",
